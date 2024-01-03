b471b69a13eb14dcec92b898745599567f746fd4
Caleb Rivera <calebrossr@gmail.com>
2024-01-03 15:52:26 -0500

    update consts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Caleb Rivera";
-export const SITE_DESCRIPTION = "Welcome to my site!";
+export const SITE_DESCRIPTION =
+  "Software engineer, cityscape enthusiast, and more.";
+
+export const SITE_URL = "https://calebrivera.com";
